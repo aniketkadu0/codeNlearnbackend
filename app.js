67a8714fd835c6cd306e50767e0970960599c49c
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-14 17:20:07 +0530

    try on cyclic.sh

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const app = express();
-
 const userRoute = require("./routes/userRoute");
-
 const bodyParser = require("body-parser");
-
 const cors = require("cors");
-
+require("./database/db");
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
-
 app.use("/user", userRoute);
+
+app.listen(process.env.PORT || 3000)
 
 module.exports = app;

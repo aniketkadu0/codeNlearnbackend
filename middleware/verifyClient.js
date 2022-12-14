cf014eef5b9ea065aa6d44174f830dfe2f108aaf
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-14 18:33:25 +0530

    final

--- middleware/verifyClient.js
+++ middleware/verifyClient.js
@@ -1,15 +1,25 @@
 const verifyClient = (req, res, next) => {
-    const credential = req.body;
-    if (!credential) return res.status(400).send("access denied");
-  
-    try {
-      const verifiedUser = credential.username == 'client' && credential.password == 'baby' ? true : false
-      if(verifiedUser){
-      next();
-      }
-      else throw new Exception();
-    } catch (err) {
-      res.status(400).send("invalid credentials");
+    var authheader = req.headers.authorization;
+ 
+    if (!authheader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err)
+    }
+ 
+    var auth = new Buffer.from(authheader.split(' ')[1],
+    'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+ 
+    if (user == 'client' && pass == 'baby') {
+        next();
+    } else {
+        var err = new Error('invalid credentials!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
     }
 };
 

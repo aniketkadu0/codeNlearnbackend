b6881ea057db59956dc3abb0ea1bd2d7f80dc654
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-14 17:40:39 +0530

    again

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
+
 const userRoute = require("./routes/userRoute");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("./database/db");
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
 app.use("/user", userRoute);
 
-app.listen(process.env.PORT || 3000)
-
-// module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-// const http = require("http");
-// const app = require("./app");
-// const server = http.createServer(app);
-// require("./database/db");
-
-// if (process.env.NODE_ENV !== "production"){
-//   require("dotenv").config();
-// }
-
-// const port = process.env.PORT || 8000;
-
-// server.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-const express = require("express");
-const app = express();
-const userRoute = require("./routes/userRoute");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
 require("./database/db");
 
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use("/user", userRoute);
+if (process.env.NODE_ENV !== "production"){
+  require("dotenv").config();
+}
 
-app.listen(process.env.PORT || 3000)
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 

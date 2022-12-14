62269c83c7dfebfd7ff6372330d4dcfc08ae7e2a
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-14 17:31:38 +0530

    again try

--- database/db.js
+++ database/db.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-
 const database = mongoose.connect(
     process.env.DB_URL,
     {   useNewUrlParser: true,
         useUnifiedTopology: true
-    },
-    (error) => {
-        if (!error){
-            console.log("connected to mongoDB!");
-        }else{
-            console.log("connection to mongoDB failed \n" + error);
-        }
     }
+    // (error) => {
+    //     if (!error){
+    //         console.log("connected to mongoDB!");
+    //     }else{
+    //         console.log("connection to mongoDB failed \n" + error);
+    //     }
+    // }
 );
 
 module.exports = database;
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
+// const http = require("http");
+// const app = require("./app");
+// const server = http.createServer(app);
+// require("./database/db");
+
+// if (process.env.NODE_ENV !== "production"){
+//   require("dotenv").config();
+// }
+
+// const port = process.env.PORT || 8000;
+
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/userRoute");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require("./database/db");
 
-if (process.env.NODE_ENV !== "production"){
-  require("dotenv").config();
-}
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/user", userRoute);
 
-const port = process.env.PORT || 8000;
-
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(process.env.PORT || 3000)
 

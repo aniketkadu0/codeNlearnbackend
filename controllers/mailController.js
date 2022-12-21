2e88a48e1f17e155ecbf7dfec6e386afb9837ea6
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-21 12:39:40 +0530

    mail change

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -14,8 +14,8 @@ async function mailer(req, res, next) {
   });
 
   await transporter.sendMail({
-    from: `"${req.body.firstName} ${req.body.lastName}" <admin@cceestudy.online>`, 
-    to: "admin@cceestudy.online", 
+    from: `"${req.body.firstName} ${req.body.lastName}" <admin@codenlearn.online>`, 
+    to: "comment@codenlearn.online", 
     subject: `${req.body.firstName} ${req.body.lastName} commented on your blog`, 
     html : `<h4>${req.body.firstName} ${req.body.lastName} commented on ${req.query.title}</h4>
             <h4>Email : </h4><a href="mailto:${req.body.email}" target="_blank">${req.body.email}</a>

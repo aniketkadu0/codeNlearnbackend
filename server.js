8bfd7acfd642d8285ee6d191930128fa8d6bbcad
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-26 15:14:19 +0530

    minor change

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ mongoose.set("strictQuery", false);
 const app = require("./app");
 const port = process.env.PORT || 8000;
 
-const database = mongoose.connect(
+const database = async () => {
+  try {
+    mongoose.connect(
     process.env.DB_URL,
     {   useNewUrlParser: true,
         useUnifiedTopology: true
@@ -16,8 +18,12 @@ const database = mongoose.connect(
         }else{
             console.log("connection to mongoDB failed \n" + error);
         }
-    }
-);
+    })
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 database().then(() => {
     app.listen(port, () => {

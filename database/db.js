04e1484e88b6b7dd10ab4c9dc7097bd5b819517b
aniketkadu0 <aniketkadu765@gmail.com>
2022-12-26 15:11:11 +0530

    mongo change

--- database/db.js
+++ database/db.js
@@ -1,18 +1,27 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
-mongoose.set("strictQuery", false);
-const database = mongoose.connect(
-    process.env.DB_URL,
-    {   useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    (error) => {
-        if (!error){
-            console.log("connected to mongoDB!");
-        }else{
-            console.log("connection to mongoDB failed \n" + error);
-        }
-    }
-);
+// const mongoose = require("mongoose");
+// require('dotenv').config();
+// mongoose.set("strictQuery", false);
 
-module.exports = database;
+// const app = require("./app");
+// const port = process.env.PORT || 8000;
+
+// const database = mongoose.connect(
+//     process.env.DB_URL,
+//     {   useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     },
+//     (error) => {
+//         if (!error){
+//             console.log("connected to mongoDB!");
+//         }else{
+//             console.log("connection to mongoDB failed \n" + error);
+//         }
+//     }
+// )
+// .then(() => {
+//     app.listen(port, () => {
+//         console.log("listening for requests");
+//     })
+// })
+
+// module.exports = database;
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-const http = require("http");
+const mongoose = require("mongoose");
+require('dotenv').config();
+mongoose.set("strictQuery", false);
+
 const app = require("./app");
-const server = http.createServer(app);
-require("./database/db");
-
-if (process.env.NODE_ENV !== "production"){
-  require("dotenv").config();
-}
-
 const port = process.env.PORT || 8000;
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const database = mongoose.connect(
+    process.env.DB_URL,
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (error) => {
+        if (!error){
+            console.log("connected to mongoDB!");
+        }else{
+            console.log("connection to mongoDB failed \n" + error);
+        }
+    }
+);
+
+database().then(() => {
+    app.listen(port, () => {
+        console.log("listening for requests");
+    })
+})
+
+// const http = require("http");
+// const app = require("./app");
+// const server = http.createServer(app);
+// require("./database/db");
+
+// if (process.env.NODE_ENV !== "production"){
+//   require("dotenv").config();
+// }
+
+// const port = process.env.PORT || 8000;
+
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
 
